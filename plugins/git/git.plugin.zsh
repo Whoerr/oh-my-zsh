3c64bb2ff9659032c4beb3704406a228a463cba1
Sam Figueroa <unimatrixzxero@gmail.com>
2013-06-18 09:51:50 +0200

    Add alias gfp for 'git fetch --prune'

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -38,6 +38,7 @@ alias gm='git merge'
 compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
+alias gfp='git fetch --prune'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
