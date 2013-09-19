dfc7cee8dc6224ed992ace33914378b508c24a60
Sam Figueroa <unimatrixzxero@gmail.com>
2013-09-19 15:28:58 +0200

    Add command with alias 'bj' to jump into gem directory
    - BJ I know, I know

--- plugins/bundler/bundler.plugin.zsh
+++ plugins/bundler/bundler.plugin.zsh
@@ -4,6 +4,7 @@ alias bl="bundle list"
 alias bp="bundle package"
 alias bo="bundle open"
 alias bu="bundle update"
+alias bj="_bundle_jump"
 
 # The following is based on https://github.com/gma/bundler-exec
 
@@ -29,6 +30,14 @@ _run-with-bundler() {
     bundle exec $@
   else
     $@
+  fi
+}
+
+_bundle_jump() {
+  if _bundler-installed && _within-bundled-project; then
+    cd `bundle show $1`
+  else
+    echo 'No in bundled dir.'
   fi
 }
 
